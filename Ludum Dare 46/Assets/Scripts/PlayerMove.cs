dd521f7aa6e768376fb9edfde9c1abc9d0672d20
krasimir <cgb18164@strath.ac.uk>
2020-04-18 12:50:25 +0100

    Add player movement

---
+++ Ludum Dare 46/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+
+    private Vector2 _direction;
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _direction.x = Input.GetAxisRaw("Horizontal");
+        _direction.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _direction * speed * Time.fixedDeltaTime);
+    }
+}

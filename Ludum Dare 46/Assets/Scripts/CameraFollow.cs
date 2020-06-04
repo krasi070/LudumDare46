a2ad43fbb05b3c74164e90dfd0da5867b1d5a9c2
krasi070 <krasi0707@gmail.com>
2020-06-04 19:20:58 +0100

    Add CameraFollow script

---
+++ Ludum Dare 46/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    [Range(0f, 1f)]
+    public float smoothing;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothing);
+    }
+}
